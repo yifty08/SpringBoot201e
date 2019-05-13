20ab963f3f37e0b36b0fe21dc0b2efc50cf5c204
Yifty <yifty08@icloud.com>
2019-05-13 16:30:39 -0400

    example

---
+++ src/main/java/com/example/springboot201e/Springboot201eApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot201e;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot201eApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot201eApplication.class, args);
+	}
+
+}
